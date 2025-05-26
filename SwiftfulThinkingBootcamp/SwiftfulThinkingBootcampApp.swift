66adc2939ad9f1db7fcf4e817d1b343a41a2c20a
weijian.duan <weijianduan0302@gmail.com>
2025-05-26 22:03:31 +0800

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Weijian Duan on 2025/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
